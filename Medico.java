fd8eeca6d8c5a963a0b70f1a2a3b7d04229f60f4
Marcelo Oliveira <72656282+NHS4002@users.noreply.github.com>
2025-04-20 20:53:46 -0300

    Delete Medico.java

--- Medico.java
+++
@@ -1,9 +0,0 @@
-package Hospital;
-
-public class Medico extends Usuario {
-    String especialidade;
-    Medico(String nome, String cpf, String senha, String especialidade) {
-        super(nome, cpf, senha);
-        this.especialidade = especialidade;
-    }
-}
